5dac610a958f48eb6dcf70d52374d89495e6fc5f
Matt Quiros <m.quiros@speedwell.com.au>
2014-12-10 16:08:24 +0800

    Added custom period cell

---
+++ Spare/Views/Table View Cells/CustomPeriodCell/CustomPeriodCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPeriodCell.swift
+//  Spare
+//
+//  Created by Matt Quiros on 12/10/14.
+//  Copyright (c) 2014 Matt Quiros. All rights reserved.
+//
+
+import Foundation
+
+class CustomPeriodCell: UITableViewCell {
+    
+    @IBOutlet weak var checkLabel: UILabel!
+    @IBOutlet weak var periodLabel: UILabel!
+    @IBOutlet weak var changeButton: UIButton!
+    
+}
